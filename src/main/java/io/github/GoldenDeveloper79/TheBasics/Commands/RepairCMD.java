2bddff08abd9dd758857bbbab8afd055fd06bf57
Hiztree <lubez81@gmail.com>
2016-01-28 13:24:48 -0700

    Fixed More Copyright

--- src/main/java/io/github/GoldenDeveloper79/TheBasics/Commands/RepairCMD.java
+++ src/main/java/io/github/GoldenDeveloper79/TheBasics/Commands/RepairCMD.java
@@ -16,29 +16,6 @@
  *******************************************************************************/
 package io.github.GoldenDeveloper79.TheBasics.Commands;
 
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
+public class RepairCMD {
 
-import io.github.GoldenDeveloper79.TheBasics.BasicUtils;
-import io.github.GoldenDeveloper79.TheBasics.Enums.MultiPlayer;
-import io.github.GoldenDeveloper79.TheBasics.Modules.CommandModule;
-import io.github.GoldenDeveloper79.TheBasics.Player.PlayerData;
-
-public class SpawnCMD extends CommandModule
-{
-	public SpawnCMD()
-	{
-		super(new String[] {"spawn"}, 0, 0, MultiPlayer.OTHER);
-	}
-
-	public void performCommand(Player player, String[] args) 
-	{
-		PlayerData data = BasicUtils.getData(player);
-		data.initTeleport(player.getWorld().getSpawnLocation(), "&7spawn");
-	}
-
-	public void performCommand(ConsoleCommandSender console, String[] args)
-	{
-		BasicUtils.sendMessage(console, "&cYou must be a player to perform this command!");	
-	}	
 }
